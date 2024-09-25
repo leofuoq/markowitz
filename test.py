55c7f046e32a1e532d0cbb82a748093bca0d5e3b
leofuoq <112978597+leofuoq@users.noreply.github.com>
2024-09-25 09:02:36 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import yfinance as yf
-import plotly.graph_objs as go
-import matplotlib.pyplot as plt
-from scipy.optimize import minimize
 
 print('hello world')
 # Kết thúc đoạn mã 
